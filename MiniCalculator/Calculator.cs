df7635aa10e4d5e90d4b682a5b6a11634f6aceef
nisslyak <nissopodolyak@gmail.com>
2024-09-08 16:16:55 +1000

    Add project files.

---
+++ MiniCalculator/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniCalculator
+{
+    public class Calculator : ICalculator
+    {
+        ILogger Logger { get; }
+        public Calculator(ILogger logger)
+        {
+            Logger = logger;
+        }
+        public double Sum(double a, double b)
+        {
+            Logger.Event("Mini Calculator started the calculations");
+            double sum = a + b;
+            Logger.Event("Mini Calculator completed the calculations");
+            return sum;
+        }
+    }
+}
---
+++ MiniCalculator/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MiniCalculator
+{
+    internal class Program
+    {
+        static ILogger Logger { get; set; }
+        static void Main(string[] args)
+        {
+            Logger = new Logger();
+
+            try
+            {
+                Console.WriteLine("Welcome to the Mini Calculator.");
+
+                Console.WriteLine("Please enter the first number");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Please enter the second number");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                Calculator calculator = new Calculator(Logger);
+                double sum = calculator.Sum(a, b);
+                Console.WriteLine("The sum of the numbers you entered is " + sum);
+                Console.ReadKey();
+            }
+            catch (FormatException)
+            {
+                Logger.Error("Error: Invalid number entered. Please enter a numeric value.");
+            }
+            catch (OverflowException)
+            {
+                Logger.Error("Error: The number you entered is either too big or too small.");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("There was an issue during the calculations. " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Thank you for using the Mini Calculator. Come back for more calculations.");
+            }
+        }
+    }
+
+    public interface ICalculator
+    {
+        double Sum(double a, double b);
+    }
+
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+        }
+    }
+}
